1c190e745bacc8c28dfccd5544172d9f40723a67
GBLin <r04943092@ntu.edu.tw>
2015-11-19 21:11:36 +0800

    Fix rotten link & add to the list

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/hw-reference/8/dist/'
+    publicPath: '/dist/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
