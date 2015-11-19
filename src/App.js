68d1e7a40338932c2274d7847097e8b268343edb
GBLin <r04943092@ntu.edu.tw>
2015-11-20 01:01:25 +0800

    Change input to be more general

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ const Item = require('./Item');
 const NewItem = require('./NewItem');
 
 const initialState = {
-    items: [
-    {
-        movie:'2001',
-        content:'Crazy movie',
-        rate:'10'
-    },
-    {
-        movie:'memento',
-        content:'Cool',
-        rate:'8'
-    }
-    ],
+    items: [],
     newMovie: '',
     newRate: '',
     newComment: ''
@@ -30,10 +19,12 @@ class App extends React.Component {
     }
 
 
-    handleRateInput(event){
+    handleInput(event){
         const inputValue = event.target.value;
         if (event.keyCode == 13 && inputValue !== '') {
             const {items, newMovie, newRate, newComment} = this.state;
+            if (newMovie === '' || newRate === '' || newComment === '')
+                return;
             items.push({
                 movie: newMovie,
                 content: newComment,
@@ -81,16 +72,24 @@ class App extends React.Component {
             <div className="app">
                 <div className="newitem">
                     <p>Movie</p>
-                    <input type="text" className="input-movie" value={newMovie} onChange={this.handleMovieChange.bind(this)}></input>
+                    <input type="text" 
+                    className="input-movie" 
+                    value={newMovie} 
+                    onChange={this.handleMovieChange.bind(this)}
+                    onKeyDown={this.handleInput.bind(this)}></input>
                     <p>Comment</p>
-                    <input type="text" className="input-comment" value={newComment} onChange={this.handleCommentChange.bind(this)}></input>
+                    <input type="text" 
+                    className="input-comment" 
+                    value={newComment} 
+                    onChange={this.handleCommentChange.bind(this)}
+                    onKeyDown={this.handleInput.bind(this)}></input>
                     <p>Rate(1~10)</p>
                     <input 
                     type="text" 
                     className="input-rate" 
                     value={newRate} 
                     onChange={this.handleRateChange.bind(this)} 
-                    onKeyDown={this.handleRateInput.bind(this)}></input>
+                    onKeyDown={this.handleInput.bind(this)}></input>
                 </div>
                 <div className="item-list">
                     {items.map(this.rendorItems, this)}
