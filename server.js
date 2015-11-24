926b51938ba5094108dc77c3b0d69f93b695b03d
GBLin <r04943092@ntu.edu.tw>
2015-11-25 00:56:35 +0800

    server ex.

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var path = require('path');
+/*var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config');
 
 var app = express();
 var compiler = webpack(config);
-/*
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler));*/
+app.use(require('webpack-hot-middleware')(compiler));
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -23,4 +23,11 @@ app.listen(3000, 'localhost', function(err) {
   }
 
   console.log('Listening at http://localhost:3000');
-});
+});*/
+
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
